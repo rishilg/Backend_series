48b2086c49fecf1784dd17e141d6f3b2240a82ec
RishilRGopal <rishilgopal6@gmail.com>
2026-07-29 18:20:25 +0530

    mongodb connection

---
+++ src/db/connection.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+
+        console.log(
+            `MongoDB Connected!! Host : ${connectionInstance.connection.host}`
+        );
+
+    } catch (error) {
+
+        console.log("MongoDB Connection Error:", error);
+
+        process.exit(1);
+
+    }
+}
+
+export default connectDB;
